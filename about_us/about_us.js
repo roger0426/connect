6e9383dc64a17e794096a2ccf0a86978dedb0e89
choucl <clc25600@gmail.com>
2021-04-05 22:29:36 +0800

    modified: about_us.css, js, add hover animation to cards

--- about_us/about_us.js
+++ about_us/about_us.js
@@ -1,3 +1,21 @@
 //computer -> 'hover' or 'mouseenter, mouselesve'
 
 //touchscreen -> 'single touch'
+
+$(document).ready(function(){
+
+  $('.card')
+  .on('mouseenter', function(){ // mouse enter
+    $('.card-detail', this).animate({height: '100%'});
+    $('.card-bg', this).animate({height: '100%', opacity: 0.5});
+    $('.personal-name', this).animate({'padding-bottom': '2%'});
+    $('.card-detail div', this).show(); // show hidden details
+  })
+
+  .on('mouseleave', function(){ // mouse release
+    $('.card-detail', this).animate({height: '28%'});
+    $('.card-bg', this).animate({height: '28%', opacity: 0.95});
+    $('.personal-name', this).animate({'padding-bottom': 0});
+    $('.card-detail div', this).hide(); // hide details
+  })
+})
