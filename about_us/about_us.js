9402e41900ca03a3b365222543c5ebdb7650b2e5
roger0426 <roger5521610@gmail.com>
2021-04-06 00:21:03 +0800

    js:add button animation, css:modify few position to match design

--- about_us/about_us.js
+++ about_us/about_us.js
@@ -6,16 +6,24 @@ $(document).ready(function(){
 
   $('.card')
   .on('mouseenter', function(){ // mouse enter
-    $('.card-detail', this).animate({height: '100%'});
-    $('.card-bg', this).animate({height: '100%', opacity: 0.5});
-    $('.personal-name', this).animate({'padding-bottom': '2%'});
+    $('.card-detail', this).animate({height: '100%'}, 300);
+    $('.card-bg', this).animate({height: '100%', opacity: 0.7}, 300);
+    $('.personal-name', this).animate({'padding-bottom': '2%'}, 300);
     $('.card-detail div', this).show(); // show hidden details
   })
 
   .on('mouseleave', function(){ // mouse release
-    $('.card-detail', this).animate({height: '28%'});
-    $('.card-bg', this).animate({height: '28%', opacity: 0.95});
-    $('.personal-name', this).animate({'padding-bottom': 0});
-    $('.card-detail div', this).hide(); // hide details
+    $('.card-detail', this).animate({height: '28%'}, 700);
+    $('.card-bg', this).animate({height: '28%', opacity: 0.85}, 700);
+    $('.personal-name', this).animate({'padding-bottom': 0}, 700);
+    $('.card-detail div', this).hide();
+  })
+    
+  $('.button')
+  .on('mouseenter', function(){
+    $(this).css("text-decoration", "underline");
+  })
+  .on('mouseleave', function(){ // mouse release
+    $(this).css("text-decoration", "none");
   })
 })
