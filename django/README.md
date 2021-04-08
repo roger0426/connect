c61f4c588e113af22f0d8283266999d0401205e6
choucl <clc25600@gmail.com>
2021-04-08 22:32:12 +0800

    added: django branch, about_us has been working successfully

---
+++ django/README.md
@@ -0,0 +1,15 @@
+# CONNECT - Django
+
+## Important!
+Please do paste the secret key inside `settings.py`
+
+## Run server
+1. move to src file
+2. `$ python manage.py`
+
+## Do anything relate to models
+1. `$ python manage.py makemigrations`
+2. `$ python manage.py migrate`
+
+## Add admin account
+1. `$ python manage.py createsuperuser`
---
+++ django/src/static/js/about_us.js
@@ -0,0 +1,33 @@
+//computer -> 'hover' or 'mouseenter, mouseleave'
+
+//touchscreen -> 'single touch'
+
+$(document).ready(function(){
+
+  $('.card') // card animation
+  .on('mouseenter', function(){ // mouse enter
+    $('.card-detail', this).stop(true, false).animate({height: '100%'}, 400);
+    $('.card-bg', this).stop(true, false).animate({height: '100%', opacity: 0.9}, 400);
+    $('.personal-name', this).stop(true, false).animate({'padding-bottom': '2%'}, 400);
+    $('.card-detail div', this).stop(true, false).show(duration = 400); // show hidden details
+  })
+
+  .on('mouseleave', function(){ // mouse release
+    $('.card-detail', this).stop(true, false).animate({height: '28%'}, 700);
+    $('.card-bg', this).stop(true, false).animate({height: '28%', opacity: 0.85}, 700);
+    $('.personal-name', this).stop(true, false).animate({'padding-bottom': 0}, 700);
+    $('.card-detail div', this).stop(true, false).hide(duration = 700);
+  })
+
+  if (!$('card-bg').is(':animated')) { // prevent unexpected showing details
+    $('.card-detail div').hide();
+  }
+    
+  $('.button') // navbar button animation
+  .on('mouseenter', function(){
+    $(this).css("text-decoration", "underline");
+  })
+  .on('mouseout', function(){ // mouse release
+    $(this).css("text-decoration", "none");
+  })
+})
