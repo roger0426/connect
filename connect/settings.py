d8dea66e593e55b6bca24bcda5e42c88c9f51206
choucl <clc25600@gmail.com>
2021-04-10 22:27:05 +0800

    modified: settings.py

--- connect/settings.py
+++ connect/settings.py
@@ -26,10 +26,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-   'connectuniverse.herokuapp.com/',
-   'localhost',
-   '127.0.0.1',
-   '0.0.0.0'
+   '*',
 ]
 
 
