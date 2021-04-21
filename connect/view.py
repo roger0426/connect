0f832343f89c756bf3f740c30818e62a78bf7685
choucl <clc25600@gmail.com>
2021-04-22 00:34:45 +0800

    doing login data link to database, still figuring out why POST is not working

---
+++ connect/view.py
@@ -0,0 +1,20 @@
+from django.contrib import auth
+from django.shortcuts import render
+
+def login(request, *args, **kwargs):
+
+  if request.user.is_authenticated:
+    return HttpResponseRedirect('/')
+
+  username = request.POST.get('username', '')
+  password = request.POST.get('password', '')
+
+  print(request.POST.get('username', ''))
+
+  user = auth.authenticate(username=username, password=password)
+
+  if user is not None and user.is_active:
+    auth.login(request, user)
+    return HttpResponseRedirect('/')
+  else:
+    return render(request, 'login.pug', {})
