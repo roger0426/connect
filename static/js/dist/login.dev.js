543ee2aebd21b6311cb7007827ee4c39b3248b32
e24089046 <wenchilin0217@gmail.com>
2021-05-02 11:17:21 +0800

    homepage.jsO

--- static/js/dist/login.dev.js
+++ static/js/dist/login.dev.js
@@ -1,40 +1,39 @@
-$(document).ready(function(){
+"use strict";
+
+$(document).ready(function () {
   console.log('in');
-  $('#sign-up')
-  .on('click touch', function(){ 
+  $('#sign-up').on('click touch', function () {
     $('#login-panel').hide();
     $('#sign-up-panel').show();
     console.log('here');
-  })
-
-  $('#login') 
-  .on('click touch', function(){ 
+  });
+  $('#login').on('click touch', function () {
     $('#login-panel').show();
     $('#sign-up-panel').hide();
-  })
-
-  $('#login-btn')
-  .on('click touch', function(){
-    $('#white-bubble1').animate({width: '1000%', height: '1000%'}, 500);
+  });
+  $('#login-btn').on('click touch', function () {
+    $('#white-bubble1').animate({
+      width: '1000%',
+      height: '1000%'
+    }, 500);
     $('#white-bubble1').css('position', 'relative');
     $('#white-bubble1').css('z-index', 1000);
-  })
+  });
+  $('#sign-up-btn').on('click touch', function () {
+    console.log($('#terms').is(':checked'));
 
-  $('#sign-up-btn')
-  .on('click touch', function(){
-    console.log($('#terms').is(':checked'))
-    if (!($('#terms').is(':checked'))) {
+    if (!$('#terms').is(':checked')) {
       alert('Please check your have accepted the terms.');
     }
+
     if (!($('#sign-pwd').val() === $('#double-pwd').val())) {
       alert('Please check your have typed correct password twice');
     }
-  })
-  
-})
+  });
+}); // check password validaty dynamically
 
-// check password validaty dynamically
-let intervalId = window.setInterval(function(){ // check every 0.5 seconds
+var intervalId = window.setInterval(function () {
+  // check every 0.5 seconds
   if ($('#double-pwd').val().length > 0) {
     if ($('#sign-pwd').val() === $('#double-pwd').val()) {
       $('#err-msg').hide();
@@ -45,17 +44,10 @@ let intervalId = window.setInterval(function(){ // check every 0.5 seconds
       $('#err-msg').text("喔不，你的密碼好像打錯了！");
     }
   }
-  if (!($('#terms').is(':checked')) ||
-      $('#double-pwd').val().length == 0 ||
-      $('#email').val().length == 0 ||
-      $('#sid').val().length == 0) {
+
+  if (!$('#terms').is(':checked') || $('#double-pwd').val().length == 0 || $('#email').val().length == 0 || $('#sid').val().length == 0) {
     $('#sign-up-btn').prop('disabled', true);
   } else {
     $('#sign-up-btn').prop('disabled', false);
   }
-
 }, 500);
-
-
-
-
