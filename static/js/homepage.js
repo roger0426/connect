a4232eaa631af1cb0c26ab5f829ff2eae26a980b
roger0426 <roger5521610@gmail.com>
2021-05-03 19:30:26 +0800

    new profile page

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,40 +1,42 @@
-$("#gotop").click(function(){
-  jQuery("html,body").animate({
-      scrollTop:0
-  },1000);
-});
-$(window).scroll(function() {
-  if ( $(this).scrollTop() > 300){
-      $('#gotop').fadeIn("fast");
-  } else {
-      $('#gotop').stop().fadeOut("fast");
-  }
-});
+$(document).ready(function(){
+  $("#gotop").click(function(){
+    jQuery("html,body").animate({
+        scrollTop:0
+    },1000);
+  });
+  $(window).scroll(function() {
+    if ( $(this).scrollTop() > 300){
+        $('#gotop').fadeIn("fast");
+    } else {
+        $('#gotop').stop().fadeOut("fast");
+    }
+  });
 
-var toggle = true;
-$("#mark").click(function() {
-	if (toggle) {
-		$("img").attr("src", "./橘色的書籤")
-		toggle = false;
-	} else {
-		$("img").attr("src", "./白色的書籤")
-		toggle = true;
-	}
-});
+  var toggle = true;
+  $("#mark").click(function() {
+    if (toggle) {
+      $("img").attr("src", "./橘色的書籤")
+      toggle = false;
+    } else {
+      $("img").attr("src", "./白色的書籤")
+      toggle = true;
+    }
+  });
 
 
-$("#title").click(function() {
-  console.log("click title");
-  $("#eventwindow").toggle();
-  $("#sent").toggle();
-});
+  $("#title").click(function() {
+    console.log("click title");
+    $("#eventwindow").toggle();
+    $("#sent").toggle();
+  });
 
-$("#sent").click(function() {
-  console.log("click sent");
-  $("#sent").hide();
-});
+  $("#sent").click(function() {
+    console.log("click sent");
+    $("#sent").hide();
+  });
 
-$("#applybutton").click(function() {
-  console.log("click apply");
-  $("#sent").show();
-});
+  $("#applybutton").click(function() {
+    console.log("click apply");
+    $("#sent").show();
+  });
+})
