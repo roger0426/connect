af36d33ac4f295abba82213e50e0c3ed6e7fe035
roger0426 <roger5521610@gmail.com>
2021-05-06 00:42:16 +0800

    modify event window

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -21,14 +21,16 @@ $(document).ready(function(){
       $("img").attr("src", "./白色的書籤")
       toggle = true;
     }
-  })
+  });
 
 
   $("#title").click(function() {
     console.log("click title");
-    $("#eventwindow").toggle();
-    $("#sent").toggle();
-  })
+    $("#eventwindow").show();
+    //$("#sent").toggle();
+  });
+  
+  
 
   $("#applybutton").click(function() {
     console.log("click apply");
@@ -36,11 +38,30 @@ $(document).ready(function(){
     $('#sent').css({"z-index": 1});
     $("#sent").animate({opacity: 1}, 500, function() {
     })
-  })
+  });
   
   $("#sent").click(function() {
     console.log("click sent");
     $("#sent").css({opacity: 0, "z-index": -1});
-  })
+  });
+  
+  
+  //按window外，關閉event window
+  /* Mark 1 的原理：
+  判断点击事件发生在区域外的条件是：
+  1. 点击事件的对象不是目标区域本身
+  2. 事件对象同时也不是目标区域的子元素
+  */
+  $(document).click(function(e){
+    var _con = $('#eventwindow');   // 设置目标区域
+    if(!_con.is(e.target) && _con.has(e.target).length === 0){ // Mark 1
+      // 功能代码
+      console.log("click !eventwindow");
+      $("#sent").css({opacity: 0, "z-index": -1});
+      $("#eventwindow").hide();
+      $("#sent").hide();
+    }
+  });
+  
 
 })
