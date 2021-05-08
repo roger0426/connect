64a955d7bed1ec230758abf186de67eff830bb7f
roger0426 <roger5521610@gmail.com>
2021-05-08 13:35:56 +0800

    modify profile page

--- static/js/profile.js
+++ static/js/profile.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
     $('#activities').show();
     $('#projects').hide();
     $('#personal').hide();
+    $('#indextag :nth-child(1)').css({opacity: 1})
+    $('#indextag :nth-child(2)').css({opacity: 0.2})
+    $('#indextag :nth-child(3)').css({opacity: 0.2})
+    $('#indextag :nth-child(4)').css({opacity: 0.2})
+    $('#indextag :nth-child(5)').css({opacity: 0.2})
   });
 
   $('#tab-navbar :nth-child(2)')
@@ -11,12 +16,47 @@ $(document).ready(function(){
     $('#activities').hide();
     $('#projects').show();
     $('#personal').hide();
+    $('#indextag :nth-child(2)').css({opacity: 1})
+    $('#indextag :nth-child(1)').css({opacity: 0.2})
+    $('#indextag :nth-child(3)').css({opacity: 0.2})
+    $('#indextag :nth-child(4)').css({opacity: 0.2})
+    $('#indextag :nth-child(5)').css({opacity: 0.2})
   });
+  
   $('#tab-navbar :nth-child(3)')
   .on('click touch', function(){ 
     $('#activities').hide();
     $('#projects').hide();
     $('#personal').show();
+    $('#indextag :nth-child(3)').css({opacity: 1})
+    $('#indextag :nth-child(1)').css({opacity: 0.2})
+    $('#indextag :nth-child(2)').css({opacity: 0.2})
+    $('#indextag :nth-child(4)').css({opacity: 0.2})
+    $('#indextag :nth-child(5)').css({opacity: 0.2})
+  });
+  
+  $('#tab-navbar :nth-child(4)')
+  .on('click touch', function(){
+    $('#activities').hide();
+    $('#projects').hide();
+    $('#personal').hide();
+    $('#indextag :nth-child(4)').css({opacity: 1})
+    $('#indextag :nth-child(1)').css({opacity: 0.2})
+    $('#indextag :nth-child(2)').css({opacity: 0.2})
+    $('#indextag :nth-child(3)').css({opacity: 0.2})
+    $('#indextag :nth-child(5)').css({opacity: 0.2})
+  });
+  
+  $('#tab-navbar :nth-child(5)')
+  .on('click touch', function(){
+    $('#activities').hide();
+    $('#projects').hide();
+    $('#personal').hide();
+    $('#indextag :nth-child(5)').css({opacity: 1})
+    $('#indextag :nth-child(1)').css({opacity: 0.2})
+    $('#indextag :nth-child(2)').css({opacity: 0.2})
+    $('#indextag :nth-child(3)').css({opacity: 0.2})
+    $('#indextag :nth-child(4)').css({opacity: 0.2})
   });
   
 })
