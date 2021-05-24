746e6170f14d020349cd876ab2e1da4dfe557ba5
roger0426 <roger5521610@gmail.com>
2021-05-25 02:10:48 +0800

    move event_detail back to homepage

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -162,12 +162,123 @@ function event_handler(URL, id, CSRF) {
       console.log("subtitle:", data.subtitle);
       console.log("image:", data.image);
       
+      if(data.image != null) {
+        $("#eventwindow" + " #eventpic" + " img").attr('src', data.image);
+        $("#eventwindow" + " #eventpic" + " p").hide();
+      }
+      else {
+        $("#eventwindow" + " #eventpic" + " img").hide();
+        $("#eventwindow" + " #eventpic" + " p").show();
+      }
+      $("#eventwindow" + " #event-title").html(data.title);
+      $("#eventwindow" + " #event-subtitle").html(data.subtitle);
+      $("#eventwindow" + " #description" + " #event-date").html("活動日期<br />" + data.event_date);
+      $("#eventwindow" + " #event-detail").html(data.detail);
+      
+      
+      
     },
     error: function(data) {
       console.log("ajax error");
     }
   })
 };
+
+
+function like_handler(URL, CSRF, event_id) {
+  $.ajaxSetup({
+    data: {
+      csrfmiddlewaretoken: CSRF
+    }
+  });
+  $.ajax({
+    type: 'post',
+    url: URL,
+    data: {
+      event_id: event_id,
+    },
+    dataType: 'json',
+    success: function(data) {
+      if (data.status == 200) {
+        console.log('success')
+        let img_url = data.user_img_url;
+        let hostname = $(location).attr('hostname');
+
+        if (data.add) {
+          let profile_url = hostname + '/profile/' + data.user_id
+          let str = "<a herf='" + profile_url + "'>\
+          <img class='member interested' src= " + img_url + "></a>";
+          console.log(str);
+          $('#likebutton').css("background-image", "url(/static/file/like-bg-y.png");
+          $('#likebutton button').attr("src", "/static/file/like-bg-y.png");
+          $('#member').append(str);
+
+        } else if (data.remove) {
+          let user_selector = "#member a img[src='" + img_url + "']";
+          console.log(user_selector)
+          $('#likebutton').css("background-image", "url(/static/file/like-bg-n.png");
+          $('#likebutton button').attr("src", "/static/file/like-bg-n.png");
+          $(user_selector).remove();
+        }
+      } else {
+        console.log("[Error]: like ajax error");
+      }
+    }
+  })
+}
+
+function message_handler(URL, CSRF) {
+  $.ajaxSetup({
+    data: {
+      csrfmiddlewaretoken: CSRF
+    }
+  });
+  $.ajax({
+    type: 'post',
+    url: URL,
+    data: {
+      'text': $("input.eventmsg-insert").val()
+    },
+    dataType: 'json',
+    success: function(data) {
+      if($(".eventmsg #eventmsg-right #eventmsg-righttop p#event-date").text() == '目前沒有留言喔～') {
+        $("#eventmsg-board").empty();
+      }
+//      let str = "<div class='eventmsg'> <a> <img class='sender' src='" + data.author_img_url +
+//          "'></img></a><div id='eventmsg-right'><div id='eventmsg-righttop'><a id='eventmsg-sendername'>" +
+//          data.author_name + "</a><p id='event-date'>" + data.msg_date + "</p></div><div id='eventmsg-rightbottom'>" +
+//          "<p id='eventmsgtext'>" + $("input.eventmsg-insert").val() + "</p></div></div>";
+//      console.log(str);
+      
+      
+      var clone_id = duplicate_multi('eventmsg');
+      console.log(clone_id);
+      //$('#' + clone_id).css({'background-color': 'red'});
+      $('#' + clone_id + " img").attr('src', data.author_img_url);
+      $('#' + clone_id + ' #eventmsg-sendername').html(data.author_name);
+      $('#' + clone_id + ' #event-date').html(data.msg_date);
+      $('#' + clone_id + " #eventmsgtext").html($("input.eventmsg-insert").val());
+      console.log($("input.eventmsg-insert").val());
+      
+      
+      //$("#eventmsg-board").prepend(str);
+      
+      $("input.eventmsg-insert").val("");
+    }
+  })
+}
+
+var i = 0;
+function duplicate_multi(duplicateID) {
+  console.log("duplicate eventpost");
+  var original = document.getElementById(duplicateID);
+  var clone = original.cloneNode(true); // "deep" clone
+  clone.id = duplicateID + '_new' + ++i;
+  // or clone.id = ""; if the divs don't need an ID
+  original.parentNode.insertBefore(clone, original.parentNode.firstChild);
+  return clone.id
+}
+
 
 function duplicate(duplicateID) {
   console.log("duplicate eventpost");
