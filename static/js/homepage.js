4615ccb68aca22b0813bd2468ff0726c7cabc746
roger0426 <roger5521610@gmail.com>
2021-05-29 02:23:50 +0800

    add force directed graph, and codepen fancy animation

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -329,3 +329,537 @@ function duplicate(duplicateID) {
   original.parentNode.insertBefore(clone, original.parentNode.firstChild);
   //$("eventpost").parent.prepend();
 };
+
+
+/*
+  畫關係force directed graph
+ ------------------------------------------------------
+ */
+
+
+//var width = window.innerWidth * 0.7
+//    height = window.innerHeight * 0.5;
+//
+//var links = [
+//             {source: 'A', target: 'B'},
+////             {source: 'B', target: 'C'},
+//             {source: 'C', target: 'A'},
+////             {source: 'D', target: 'A'},
+//             {source: 'A', target: 'D'},
+//             {source: 'C', target: 'H'},
+//             {source: 'A', target: 'E'},
+//             {source: 'A', target: 'G'},
+//
+//             ];
+//
+//var nodes = {};
+//
+////parse links to nodes
+//links.forEach(function(link) {
+//  link.source = nodes[link.source] ||
+//      (nodes[link.source] = {name: link.source});
+//  link.target = nodes[link.target] ||
+//      (nodes[link.target] = {name: link.target});
+//});
+//
+////var canvas = document.getElementById("relation_graph")
+//////    .attr('width', width)
+//////    .attr('height', height);
+////const relation_graph = canvas.getContext('2d');
+//
+//var svg = d3.select('#board').append('svg')
+//    .attr('id', 'relation_graph')
+//    .attr('position', 'fixed')
+////    .attr('width', width)
+////    .attr('height', height)
+//    .attr('width', '100%');
+//
+//var force = d3.layout.force()
+//    .size([width, height])
+//    .nodes(d3.values(nodes))
+//    .links(links)
+//    .on("tick", tick)
+//    .charge(-1500)
+//    .gravity(0.15)
+//    .friction(0.8)
+//    .linkDistance(100)
+//    .start();
+//
+//var link = svg.selectAll('.link')
+//    .data(links)
+//    .enter().append('line')
+//    .attr('class', 'link');
+//
+//var node = svg.selectAll('node')
+//    .data(force.nodes())
+//    .enter().append('circle')
+//    .attr('class', 'node')
+//    .attr('r', width * 0.01);
+//
+//function tick(e) {
+//  node.attr('cx', function(d) { return d.x; })
+//      .attr('cy', function(d) { return d.y; })
+//      .call(force.drag);
+//
+//  link.attr('x1', function(d) { return d.source.x; })
+//      .attr('y1', function(d) { return d.source.y; })
+//      .attr('x2', function(d) { return d.target.x; })
+//      .attr('y2', function(d) { return d.target.y; })
+//}
+
+//另一個版本
+
+var map=[
+	{
+		name:"臺北市"
+	},
+	
+	{
+		name:"新北市"
+	},
+	
+	{
+		name:"桃園市"
+	},
+	
+	{
+		name:"臺中市"
+	},
+	
+	{
+		name:"臺南市"
+	},
+	
+	{
+		name:"高雄市"
+	},
+	
+	{
+		name:"基隆市"
+	},
+	
+	{
+		name:"新竹市"
+	},
+	
+	{
+		name:"嘉義市"
+	},
+	
+	{
+		name:"新竹縣"
+	},
+	
+	{
+		name:"苗栗縣"
+	},
+	
+	{
+		name:"彰化縣"
+	},
+	
+	{
+		name:"南投縣"
+	},
+	
+	{
+		name:"雲林縣"
+	},
+	
+	{
+		name:"嘉義縣"
+	},
+	
+	{
+		name:"屏東縣"
+	},
+	
+	{
+		name:"宜蘭縣"
+	},
+	
+	{
+		name:"花蓮縣"
+	},
+	
+	{
+		name:"臺東縣"
+	},
+];
+var links=[
+	{
+		source:0,
+	 	target:1
+	},
+	{
+		source:0,
+	 	target:6
+	},
+	{
+		source:1,
+	 	target:2
+	},
+	{
+		source:1,
+	 	target:16
+	},
+	{
+		source:2,
+	 	target:9
+	},
+	{
+		source:2,
+	 	target:16
+	},
+	{
+		source:3,
+	 	target:9
+	},
+	{
+		source:3,
+	 	target:10
+	},
+	{
+		source:3,
+	 	target:11
+	},
+	{
+		source:3,
+	 	target:12
+	},
+	{
+		source:3,
+	 	target:16
+	},
+	{
+		source:3,
+	 	target:17
+	},
+	{
+		source:4,
+	 	target:5
+	},
+	{
+		source:4,
+	 	target:14
+	},
+	{
+		source:5,
+	 	target:12
+	},
+	{
+		source:5,
+	 	target:14
+	},
+	{
+		source:5,
+	 	target:15
+	},
+	{
+		source:5,
+	 	target:17
+	},
+	{
+		source:5,
+	 	target:18
+	},
+	{
+		source:7,
+	 	target:10
+	},
+	{
+		source:7,
+	 	target:11
+	},
+	{
+		source:8,
+	 	target:14
+	},
+	{
+		source:9,
+	 	target:10
+	},
+	{
+		source:9,
+	 	target:16
+	},
+	{
+		source:11,
+	 	target:12
+	},
+	{
+		source:11,
+	 	target:13
+	},
+	{
+		source:12,
+	 	target:13
+	},
+	{
+		source:12,
+	 	target:14
+	},
+	{
+		source:12,
+	 	target:17
+	},
+	{
+		source:13,
+		target:14
+	},
+	{
+		source:15,
+	 	target:18
+	},
+	{
+		source:16,
+	 	target:17
+	},
+	{
+		source:17,
+	 	target:18
+	},
+];
+
+
+var width = window.innerWidth * 0.7
+    height = window.innerHeight * 0.5;
+
+var svg = d3.select('#board').append('svg')
+    .attr('id', 'relation_graph')
+    .attr('position', 'fixed')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('width', '100%');
+
+
+var simulation = d3.forceSimulation()
+	.force("link", d3.forceLink())
+    .force("charge", d3.forceManyBody(-500))
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+var link = svg.append("g")
+	.attr("class", "links")
+	.selectAll("line")
+	.data(links)
+	.enter().append("line")
+	.attr("stroke-width", 2)
+	.attr("stroke","black");
+
+var node = svg.append("g")
+	.attr("class", "nodes")
+	.selectAll("circle")
+	.data(map)
+	.enter().append("circle")
+	.attr("r", 8)
+	.attr("fill", 'black')
+	.attr('stroke','white')
+	.attr('stroke-width',2)
+	.call(d3.drag()
+		.on("start", dragstarted)
+		.on("drag", dragged)
+		.on("end", dragended));
+
+var text = svg.selectAll("text")
+     .data(map)
+     .enter()
+     .append("text")
+     .style("fill", "black")
+     .attr("dx", 12)
+     .attr("dy", 5)
+     .text(function(d){
+        return d.name;
+     });
+
+simulation
+	.nodes(map)
+	.on("tick", ticked);
+
+simulation.force("link")
+	.links(links)
+	.distance(50);
+
+simulation.force("charge")
+	.strength(-60)
+function ticked() {
+link
+	.attr("x1", function(d) { return d.source.x; })
+	.attr("y1", function(d) { return d.source.y; })
+	.attr("x2", function(d) { return d.target.x; })
+	.attr("y2", function(d) { return d.target.y; });
+
+node
+	.attr("cx", function(d) { return d.x; })
+	.attr("cy", function(d) { return d.y; });
+
+text
+	.attr("x", function(d) { return d.x;})
+	.attr("y", function(d) { return d.y;});
+};
+
+function dragstarted(d) {
+if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+d.fx = d.x;
+d.fy = d.y;
+}
+
+function dragged(d) {
+d.fx = d3.event.x;
+d.fy = d3.event.y;
+}
+
+function dragended(d) {
+if (!d3.event.active) simulation.alphaTarget(0);
+d.fx = null;
+d.fy = null;
+}
+
+/*
+  畫背景飄移force directed graph
+  -----------------------------------------------------
+*/
+var c = document.getElementById('world');
+var ctx = c.getContext('2d');
+//var w = window.innerWidth;
+//var h = window.innerHeight;
+var w = window.screen.width;
+var h = window.screen.height;
+//var w = $('window').width();
+//var h = $('window').height();
+
+var m = {
+  x : 0,
+  y : 0
+};
+var cdist = 200; //connect distance //100
+window.onmousedown = function(e) {
+  if(e.button == 0) {
+    p.push(new Particle(m.x , m.y));
+  }
+  
+}
+
+
+window.onmousemove = function(e) {
+  m.x = e.clientX || e.pageX || 0;
+  m.y = e.clientY || e.pageY || 0;
+}
+c.width = w;
+c.height = h;
+
+function drawBG(clr) {
+  ctx.fillStyle = clr
+ctx.fillRect(0,0,w,h);
+}
+
+
+
+function drawText(txt , x , y , size , types , color) {
+  ctx.font = size + "px" + " anton";
+  ctx.textAlign = "center";
+  ctx.lineWidth = 1;
+  if(types == "stroke") {
+    ctx.strokeStyle = color;
+    ctx.strokeText(txt , x, y);
+  } else if(types = "fill") {
+    ctx.fillStyle = color;
+    ctx.fillText(txt , x, y);
+  }
+  
+  
+}
+
+function removeParticle(p , i) {
+  window.onkeydown = function(e) {
+    if(e.keyCode == 32) {
+      p.splice(i , 1);
+    }
+  }
+}
+
+
+
+//random color generator...........
+// function randomColor() {
+//   var r = Math.floor(Math.random() * 255);
+//   var g = Math.floor(Math.random() * 255);
+//   var b = Math.floor(Math.random() * 255);
+  
+//   return "rgb(" + r + ',' + g + ',' + b + ")";
+// }
+var p = [] , cnt = 200;
+
+for(var i = 0; i < cnt; i++) {
+  p.push(new Particle());
+}
+
+function Particle(x , y) {
+  this.x = x || Math.random() * w;
+  this.y = y || Math.random() * h;
+  this.vx = (Math.random() - 0.5) * 0.6;
+  this.vy = (Math.random() - 0.5) * 0.6;
+  this.rad = 0.8;
+  this.color = "#fff"
+  
+  this.move = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+      if(this.x > w + this.rad) {
+        this.x = 0 - this.rad;
+      } else if(this.x < 0 - this.rad) {
+        this.x = w + this.rad;
+      }
+    
+    if(this.y > h + this.rad) {
+        this.y = 0 - this.rad;
+      } else if(this.y < 0 - this.rad) {
+        this.x = h + this.rad;
+      }
+  }
+  this.show = function() {
+    ctx.beginPath();
+    ctx.arc(this.x , this.y , this.rad , 0 , Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+}
+
+function connectDots(p1 , p2) {
+  var dx = p1.x - p2.x;
+  var dy = p1.y - p2.y;
+  var dist = Math.sqrt(dx * dx + dy * dy);
+  
+  if(dist <= cdist) {
+    ctx.beginPath();
+    ctx.strokeStyle = "rgba(0,0,0,1)";
+    //"rgba(200,201,250,1)";
+    ctx.lineWidth = 0.2; //0.07
+    ctx.moveTo(p1.x , p1.y);
+    ctx.lineTo(p2.x , p2.y);
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+loop();
+
+
+function loop() {
+  window.requestAnimationFrame(loop);
+  drawBG("#FFF6ED");
+  //drawText("Made With ♥ & Javascript" , w / 2 , h / 2 , 65 , "stroke" , "#fff");
+  //drawText("By suzan",w / 2 + 250 , h / 2 + 40 , 20 , "fill", "#fff")
+  for(var i = 0; i < p.length; i++) {
+    p[i].show();
+    p[i].move();
+    //removeParticle(p , 1);
+    
+    
+    connectDots(p[i],m);
+    for(var j = 0; j < p.length; j++) {
+      connectDots(p[i],p[j]);
+    }
+  }
+}
--- static/js/login.js
+++ static/js/login.js
@@ -65,3 +65,155 @@ $('#double-pwd').keyup(function(){
 
 
 
+
+
+/*
+  OK THATS IT. JUST PLAY WITH IT----
+  -------------------------
+*/
+var c = document.getElementById('world');
+var ctx = c.getContext('2d');
+//var w = window.innerWidth;
+//var h = window.innerHeight;
+var w = window.screen.width;
+var h = window.screen.height;
+//var w = $('window').width();
+//var h = $('window').height();
+
+var m = {
+  x : 0,
+  y : 0
+};
+var cdist = 150; //connect distance //100
+window.onmousedown = function(e) {
+  if(e.button == 0) {
+    p.push(new Particle(m.x , m.y));
+  }
+  
+}
+
+
+window.onmousemove = function(e) {
+  m.x = e.clientX || e.pageX || 0;
+  m.y = e.clientY || e.pageY || 0;
+}
+c.width = w;
+c.height = h;
+
+function drawBG(clr) {
+  ctx.fillStyle = clr
+ctx.fillRect(0,0,w,h);
+}
+
+
+
+function drawText(txt , x , y , size , types , color) {
+  ctx.font = size + "px" + " anton";
+  ctx.textAlign = "center";
+  ctx.lineWidth = 1;
+  if(types == "stroke") {
+    ctx.strokeStyle = color;
+    ctx.strokeText(txt , x, y);
+  } else if(types = "fill") {
+    ctx.fillStyle = color;
+    ctx.fillText(txt , x, y);
+  }
+  
+  
+}
+
+function removeParticle(p , i) {
+  window.onkeydown = function(e) {
+    if(e.keyCode == 32) { //32
+      p.splice(i , 1);
+    }
+  }
+}
+
+
+
+//random color generator...........
+// function randomColor() {
+//   var r = Math.floor(Math.random() * 255);
+//   var g = Math.floor(Math.random() * 255);
+//   var b = Math.floor(Math.random() * 255);
+  
+//   return "rgb(" + r + ',' + g + ',' + b + ")";
+// }
+var p = [] , cnt = 200;
+
+for(var i = 0; i < cnt; i++) {
+  p.push(new Particle());
+}
+
+function Particle(x , y) {
+  this.x = x || Math.random() * w;
+  this.y = y || Math.random() * h;
+  this.vx = (Math.random() - 0.5) * 0.6;
+  this.vy = (Math.random() - 0.5) * 0.6;
+  this.rad = 0.8;
+  this.color = "#fff"
+  
+  this.move = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+      if(this.x > w + this.rad) {
+        this.x = 0 - this.rad;
+      } else if(this.x < 0 - this.rad) {
+        this.x = w + this.rad;
+      }
+    
+    if(this.y > h + this.rad) {
+        this.y = 0 - this.rad;
+      } else if(this.y < 0 - this.rad) {
+        this.x = h + this.rad;
+      }
+  }
+  this.show = function() {
+    ctx.beginPath();
+    ctx.arc(this.x , this.y , this.rad , 0 , Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+}
+
+function connectDots(p1 , p2) {
+  var dx = p1.x - p2.x;
+  var dy = p1.y - p2.y;
+  var dist = Math.sqrt(dx * dx + dy * dy);
+  
+  if(dist <= cdist) {
+    ctx.beginPath();
+    ctx.strokeStyle = "#3AEDC2";
+    //"#FAAD95";
+    //"rgba(200,201,250,1)";
+    ctx.lineWidth = 0.15; //0.07
+    ctx.moveTo(p1.x , p1.y);
+    ctx.lineTo(p2.x , p2.y);
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+loop();
+
+
+function loop() {
+  window.requestAnimationFrame(loop);
+  drawBG("#FFF6ED");
+  //drawText("Made With ♥ & Javascript" , w / 2 , h / 2 , 65 , "stroke" , "#fff");
+  //drawText("By suzan",w / 2 + 250 , h / 2 + 40 , 20 , "fill", "#fff")
+  for(var i = 0; i < p.length; i++) {
+    p[i].show();
+    p[i].move();
+    removeParticle(p , 1); //1
+    
+    
+    connectDots(p[i],m);
+    for(var j = 0; j < p.length; j++) {
+      connectDots(p[i],p[j]);
+    }
+  }
+}
