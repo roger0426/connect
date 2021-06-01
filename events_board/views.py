18cb1cbec371efbea24529adb43bda123612f9f5
roger0426 <roger5521610@gmail.com>
2021-06-01 21:05:42 +0800

    fix part of layout UI probs

--- events_board/views.py
+++ events_board/views.py
@@ -87,7 +87,8 @@ def like_view(request, id):
       receiver = event.host.user
       sender = request.user
       notification = SiteNotification.objects.create(
-        text = sender.userextend.full_name + "對您的活動感到有興趣， 快去看看吧",
+        text = "對您的活動感到有興趣， 快去看看吧",
+        #sender.userextend.full_name +
         event = event,
         for_user = receiver,
         from_user = sender
