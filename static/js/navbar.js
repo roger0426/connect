356f053ba68f81c7557f22f1f843ae9119280542
choucl <clc25600@gmail.com>
2021-06-27 17:44:03 +0800

    add search to homepage in responsive UI

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -81,31 +81,31 @@ $(document).ready(function(){
   $('#selecticon').click(function(){
     if ($('#feedback').css('display') === "none"){
       $('#navbar').css({'border-radius': '0 0 0.5rem 0.5rem'});
-      $("#navbar").animate({'padding-bottom': '5rem'}, 300, function() {
+      $("#navbar").animate({'padding-bottom': '5rem'}, 150, function() {
       });
-      $("#container").animate({'padding-top': '4.5rem'}, 300, function() {
+      $("#container").animate({'padding-top': '4.5rem'}, 150, function() {
         $('#feedback, #alert, #message, #aboutus').css({'display': "block"});
       });
-      $("#profilepanel").animate({'padding-top': '4.5rem'}, 300, function() {
+      $("#profilepanel").animate({'padding-top': '4.5rem'}, 150, function() {
         $('#feedback, #alert, #message, #aboutus').css({'display': "block"});
       });
-      $("#group-img").animate({'top': '7.5rem'}, 300, function() {
+      $("#group-img").animate({'top': '7.5rem'}, 150, function() {
         $('#feedback, #alert, #message, #aboutus').css({'display': "block"});
       });
-      $("#img-cards").animate({'top': '20rem'}, 300, function() {
+      $("#img-cards").animate({'top': '20rem'}, 150, function() {
       });
     } else {
       $('#navbar').css({'border-radius': '0'});
       $('#feedback, #alert, #message, #aboutus').css({'display': "none"});
-      $("#navbar").animate({'padding-bottom': '0'}, 300, function() {
+      $("#navbar").animate({'padding-bottom': '0'}, 150, function() {
       });
-      $("#container").animate({'padding-top': '0'}, 300, function() {
+      $("#container").animate({'padding-top': '0'}, 150, function() {
       });
-      $("#profilepanel").animate({'padding-top': '0'}, 300, function() {
+      $("#profilepanel").animate({'padding-top': '0'}, 150, function() {
       });
-      $("#group-img").animate({'top': '2rem'}, 300, function() {
+      $("#group-img").animate({'top': '2rem'}, 150, function() {
       });
-      $("#img-cards").animate({'top': '14rem'}, 300, function() {
+      $("#img-cards").animate({'top': '14rem'}, 150, function() {
       });
     }
   })
