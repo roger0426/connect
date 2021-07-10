0a3d9c161cff163355f6b9be06903f34ac6ff363
roger0426 <roger5521610@gmail.com>
2021-07-10 14:29:02 +0800

    add pull to refresh to phone responsive

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -142,3 +142,53 @@ $(document).ready(function(){
 //    }
 //  })
 })
+
+var pStart = {x: 0, y:0};
+var pStop = {x:0, y:0};
+
+function swipeStart(e) {
+  if (typeof e['targetTouches'] !== "undefined"){
+      var touch = e.targetTouches[0];
+      pStart.x = touch.screenX;
+      pStart.y = touch.screenY;
+  } else {
+      pStart.x = e.screenX;
+      pStart.y = e.screenY;
+  }
+}
+
+function swipeEnd(e){
+  if (typeof e['changedTouches'] !== "undefined"){
+      var touch = e.changedTouches[0];
+      pStop.x = touch.screenX;
+      pStop.y = touch.screenY;
+  } else {
+      pStop.x = e.screenX;
+      pStop.y = e.screenY;
+  }
+
+  swipeCheck();
+}
+
+function swipeCheck(){
+  var changeY = pStart.y - pStop.y;
+  var changeX = pStart.x - pStop.x;
+  if (isPullDown(changeY, changeX) ) {
+    if($(window).width() < 480){
+      console.log('pull down reloading');
+      $('#container').css('margin-top', "4rem");
+      location.reload();
+    }else{}
+  }
+}
+
+function isPullDown(dY, dX) {
+  // methods of checking slope, length, direction of line created by swipe action 
+  return dY < 0 && (
+      (Math.abs(dX) <= 80 && Math.abs(dY) >= 100)  //300
+      || (Math.abs(dX)/Math.abs(dY) <= 0.3 && dY >= 25)   //60
+  );
+}
+
+document.addEventListener('touchstart', function(e){ swipeStart(e); }, false);
+document.addEventListener('touchend', function(e){ swipeEnd(e); }, false);
